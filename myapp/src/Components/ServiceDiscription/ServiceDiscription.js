cf151627da97ee199154e8e5ab777f5712aa21a6
Mohammodrubel <74967048+mohammodrubel@users.noreply.github.com>
2021-10-19 13:29:57 +0600

    Add files via upload

---
+++ myapp/src/Components/ServiceDiscription/ServiceDiscription.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router';
+import './ServiceDiscription.css'
+
+const ServiceDiscription = (props) => {
+     
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ServiceDiscription;
